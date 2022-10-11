70523d0707069c4db6a3c2a93d3eb73ce58dfc06
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-11 18:00:32 +0000

    added bootstrap

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import SubmitButton from './submitButton';
 import ZipInput from './ZipInput';
-// import ZipInput from './ZipInput';
 let root = ReactDOM.createRoot(document.getElementById('root'));
 // const SubButton = <SubmitButton />
-const zipField = <ZipInput />
-root.render(zipField);
+// const zipField = <ZipInput />
+// root.render(zipField);
---
+++ my-app/src/zipfield.js
@@ -0,0 +1,5 @@
+function buildField () {
+    return(
+        
+    )
+}
