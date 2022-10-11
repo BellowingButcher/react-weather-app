75417448927385c3ec7d347d0974999cac664542
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-11 17:55:21 +0000

    Added a zip input field file
    - tested and true

---
+++ my-app/src/ZipInput.js
@@ -0,0 +1,6 @@
+function ZipInput () {
+    return(
+        <input type='number'></input>
+    )
+}
+export default ZipInput;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SubmitButton from './submitButton';
+// import SubmitButton from './submitButton';
+import ZipInput from './ZipInput';
+// import ZipInput from './ZipInput';
 let root = ReactDOM.createRoot(document.getElementById('root'));
-const SubButton = <SubmitButton />
-root.render(SubButton);
+// const SubButton = <SubmitButton />
+const zipField = <ZipInput />
+root.render(zipField);
